a77911f0f6eecb7997f40d9e16df734bdb13d488
bus000 <magnus@stavngaard.dk>
2015-04-09 17:40:48 +0200

    Made program accept multiple files.

--- trailing_space.c
+++ trailing_space.c
@@ -3,40 +3,52 @@
 #include <stdlib.h>
 
 static bool is_whitespace(char c);
+static bool has_trailing(FILE *file);
 
 int main(int argc, char const *argv[])
 {
+    int i;
     FILE *file;
-    char cur_c, prev_c;
-    bool has_whitespace = FALSE;
 
     /* Test the number of arguments. */
-    if (argc != 2) {
-        fprintf(stderr, "Program expects two arguments.\n");
+    if (argc < 2) {
+        fprintf(stderr, "Program expects at least 1 argument.\n");
         return WRONG_ARG_NUMBER;
     }
 
-    /* Open file with read rights. */
-    if ((file = fopen(argv[1], "r")) == NULL) {
-        fprintf(stderr, "Could not open file %s\n", argv[1]);
-        return FILE_OPEN_ERROR;
+    for (i = 1; i < argc; i++) {
+        /* Open file with read rights. */
+        if ((file = fopen(argv[i], "r")) == NULL) {
+            fprintf(stderr, "Could not open file %s\n", argv[i]);
+        }
+
+        printf(has_trailing(file) ? "Yes\n" : "No\n");
+
+        /* Close file. */
+        fclose(file);
     }
 
-    /* Read the file char by char checking if there is ever a whitespace
-     * character before an endline character. */
-    prev_c = cur_c = 'a';
+    return EXIT_SUCCESS;
+}
+
+/* Read the file char by char checking if there is ever a whitespace character
+ * before an endline character.  Return TRUE if a trailing space is found and
+ * FALSE otherwise. */
+static bool has_trailing(FILE *file)
+{
+    char cur_c, prev_c; prev_c = cur_c = 'a';
+    bool has_trailing = FALSE;
+
     while ((cur_c = fgetc(file)) != EOF) {
         if (cur_c == '\n' && is_whitespace(prev_c)) {
-            has_whitespace = TRUE;
+            has_trailing = TRUE;
             break;
         }
 
         prev_c = cur_c;
     }
 
-    printf(has_whitespace ? "Yes\n" : "No\n");
-
-    return EXIT_SUCCESS;
+    return has_trailing;
 }
 
 static bool is_whitespace(char c)
