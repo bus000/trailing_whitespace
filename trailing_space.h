341457b75c5c167c4d6558930e397172acbc7bbb
bus000 <magnus@stavngaard.dk>
2015-04-10 18:41:09 +0200

    Added a temp filename definition

--- trailing_space.h
+++ trailing_space.h
@@ -4,10 +4,12 @@
 #include <stdlib.h>
 
 #define WRONG_ARG_NUMBER (EXIT_SUCCESS - 1)
-#define UNKNOWN_FLAG      (EXIT_SUCCESS - 2)
+#define UNKNOWN_FLAG     (EXIT_SUCCESS - 2)
 
 typedef int bool;
 #define TRUE  1
 #define FALSE 0
+
+#define TEMP_NAME ".tempfilename~"
 
 #endif
