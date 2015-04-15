7656f8039aa18668a32d1c76f326c738216ea35d
bus000 <magnus@stavngaard.dk>
2015-04-15 22:16:05 +0200

    New error message defines and wrapped old defines in parenthesis to ensure that they don't ruin anything where they are inserted.

--- trailing_space.h
+++ trailing_space.h
@@ -3,18 +3,23 @@
 
 #include <stdlib.h>
 
+/* Program error codes. */
 #define WRONG_ARG_NUMBER (EXIT_SUCCESS - 1)
 #define UNKNOWN_FLAG     (EXIT_SUCCESS - 2)
 
+/* Define boolean values. */
 typedef int bool;
 #define TRUE  1
 #define FALSE 0
 
-#define TEMP_NAME ".tempfilename~"
+/* Function error codes. */
+#define NO_ERR                ( 0)
+#define COULD_NOT_OPEN_FILE   (-1)
+#define COULD_NOT_DELETE_FILE (-2)
+#define COULD_NOT_RENAME_FILE (-3)
+#define TEMP_FILE_CREATION    (-4)
 
-#define NO_ERR                 0
-#define COULD_NOT_OPEN_FILE   -1
-#define COULD_NOT_DELETE_FILE -2
-#define COULD_NOT_RENAME_FILE -3
+/* Other definitions. */
+#define TEMP_NAME ".tempfilename~"
 
 #endif
